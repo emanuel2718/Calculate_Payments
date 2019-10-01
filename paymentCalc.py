a8402dfd38d82b266bcd7eff78ba76b39d4647ce
emanuel2718 <eramirez2718@gmail.com>
2019-10-01 19:47:26 -0400

    Added missing functions

--- paymentCalc.py
+++ paymentCalc.py
@@ -19,7 +19,6 @@ def main():
             exit()
 
 
-
 def test_file(filename):
     try:
         fhandler = open(filename, 'r')
@@ -42,7 +41,6 @@ def print_menu():
     print("6. Exit")
 
 
-
 # Verify the validity of the option chosen
 # Option must be a number between 1 and 6 inclusive
 def choose_option(option):
@@ -52,73 +50,109 @@ def choose_option(option):
         if numeric_option >= 1 and numeric_option <= 6:
             return numeric_option
         else:
-            print("Please enter a valid positive number between 1 and 6.")
-            print(input("Press Enter to continue..."))
             return -1 # Invalid option
     else:
-        print("Please enter a valid positive number between 1 and 6.")
-        print(input("Press Enter to continue..."))
         return -1 # Invalid option
 
 
-
 # Calculate the payment of an employee
-#def calculate_payment(hours, rate):
+def calculate_payment(hours, rate):
+    if hours <= 40 or hours > 50: # Regular pay
+        return hours * rate
+    else:
+        return (40 * rate) + ((hours - 40) * (rate * 1.5)) # Calculate overtime
 
 
-
-
-
-
-
-
-
-
+# Evaluate if the file to be looked at exist or not.
 def evaluate_request(option):
     filename = input("Enter the file to be evaluated: ")
     if test_file(filename):
         fhandler = open(filename, 'r') # Open the file
-        find_employee_info(option)
+        find_employee_info(filename, fhandler, option)
 
     else:
         print("Illegal file name. Input file was not found")
         print(input("Press Enter to continue: "))
         return -1
 
-    if (option == 1):
-        print("A file", filename, "containing the payment functions has been created.")
+def find_name(line):
+    return line.split(",")[0]
 
-    elif (option == 2):
-        print("The employee with the maximum number of work hours is: ")
+def find_hours(line):
+    return line.split(",")[1]
 
-    elif (option == 3):
-        print("The employee with the minimum number of work hours is: ")
-
-    elif (option == 4):
-        print("The employee with the maximum rate is: ")
-
-    elif (option == 5):
-        print("The employee with the minimum rate is: ")
-
-    else:
-        if (option == 6):
-            print("Thanks for using the payment calculation program")
-            exit()
+def find_rate(line):
+    return line.split(",")[2]
 
 
-
-
-def find_employee_info(fhandler, option):
-    newfile = open("employess_payment.txt", 'w')
+def create_hours_dict(filename, fhandler):
+    hours_dict = {}
     for line in fhandler:
-        if not line.startwith("time"):
+        if not line.startswith("time"):
+            hours_dict[find_name(line)] = find_hours(line)
+        else:
+            continue
+    return hours_dict
+
+def create_rate_dict(filename, fhandler):
+    rate_dict = {}
+    for line in fhandler:
+        if not line.startswith("time"):
+            rate_dict[find_name(line)] = find_rate(line).split("\n")[0]
+        else:
+            continue
+    return rate_dict
+
+# Avoid to have to write the print everytime
+def press_enter():
+    print(input("Press Enter to continue"))
 
 
+def find_employee_info(filename, fhandler, option):
+    if option == 1:
+        newfile = open("employees_payment.txt", 'w')
+        for line in fhandler:
+            if not line.startswith("time"):
+                newfile.write(find_name(line).ljust(20) + "$" + str(calculate_payment(float(find_hours(line)), float(find_rate(line)))) + "\n")
+            else:
+                continue
+        print("A file named employees_payment.txt, containing the payment functions has been created.")
+        press_enter()
+        newfile.close()
+        fhandler.close()
 
+    # Find the name of the employee with the highest number of hours
+    elif option == 2:
+        hours_dict = create_hours_dict(filename, fhandler)
+        sorted_dict = sorted((value, key) for (key, value) in hours_dict.items())
+        print("The employee with the max number of hours is: " + str(sorted_dict[-1][1]) + " | Expected: Elon Musk" + '\n')
+        press_enter()
 
+    # Find the name of the employee with the lowest number of hours
+    elif option == 3:
+        hours_dict = create_hours_dict(filename, fhandler)
+        sorted_dict = sorted((value, key) for (key, value) in hours_dict.items())
+        print("The employee with the min number of hours is: " + str(sorted_dict[0][1]) + " | Expected: Benjamin Franklin" + '\n')
+        press_enter()
+
+    # Find the name of the employee with the highest rate
+    elif option == 4:
+        rate_dict = create_rate_dict(filename, fhandler)
+        sorted_dict = sorted((value, key) for (key, value) in rate_dict.items())
+        print("The employee with the max rate is: " + str(sorted_dict[-1][1]) + " | Expected: Thomas Edison" + '\n')
+        press_enter()
+
+    # Find the name of the employee with the highest rate
+    elif option == 5:
+        rate_dict = create_rate_dict(filename, fhandler)
+        sorted_dict = sorted((value, key) for (key, value) in rate_dict.items())
+        print("The employee with the min rate is: " + str(sorted_dict[0][1]) + " | Expected: Nikola Tesla" + '\n')
+        press_enter()
+
+    # Exit the program
+    elif (option == 6):
+        print("Thanks for using the payment calculation program")
+        exit()
 
 if __name__ == "__main__":
     main()
-
-
-
